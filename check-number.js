5600fb1dcbd9dfa18780d3ce6bacffd405455ebb
nanaknihal <nanaknihal@gmail.com>
2022-11-18 03:13:25 -0500

    added database

--- check-number.js
+++ check-number.js
@@ -3,34 +3,29 @@ const axios = require("axios");
 const { randomBytes } = require("crypto");
 const { ethers } = require("ethers");
 const { poseidon } = require("circomlibjs-old");
-
 require("dotenv").config();
-
 const client = require("twilio")(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
+const dbConfig = process.env.NO_DB_ACCESS || require("../phone-number-db.config.js");
+const mysql = require("mysql");
 const express = require("express");
+
 const app = express();
 const port = 3030;
-
 const MAX_FRAUD_SCORE = 2; // ipqualityscore.com defines fraud score. This constant will be used to only allow phone numbers with a <= fraud score.
 
-const getIsSafe = (phoneNumber, country, callback) => {
-    assert(phoneNumber && country);
-    axios.get(`https://ipqualityscore.com/api/json/phone/${process.env.IPQUALITYSCORE_APIKEY}/${phoneNumber}?country[]=${country}`)
-    .then((response) => {
-        if(!("fraud_score" in response?.data)) {throw `Invalid response: ${JSON.stringify(response)} `}
-        callback(response.data.fraud_score <= MAX_FRAUD_SCORE)
-    })
-    
+if(!process.env.NO_DB_ACCESS){
+    const connection = mysql.createConnection({
+        host: dbConfig.HOST,
+        user: dbConfig.USER,
+        password: dbConfig.PASSWORD,
+        database: dbConfig.DB
+      });
+      connection.connect(error => {
+          if (error) throw error;
+          console.log("Successfully connected to database");
+      });
 }
 
-const getCredentials = (phoneNumber, country, callback) => {
-    assert(phoneNumber && country);
-    getIsSafe(phoneNumber, country, (isSafe) => {
-        if (!isSafe) throw "phone number could not be determined to belong to a unique human";
-        credsFromNumber(phoneNumber).then(creds => callback(creds));
-    });
-}
 
 // Sends a new code to number (E.164 format e.g. +13109273149)
 app.get("/send/:number", (req, res) => {
@@ -39,10 +34,10 @@ app.get("/send/:number", (req, res) => {
     client.verify.v2.services(process.env.TWILIO_SERVICE_SID)
                 .verifications
                 .create({to: req.params.number, channel: "sms"})
-                .then(verification => res.send(verification));
+                .then(() => res.status(200));
 })
 
-// Checks that user-provided code is the one that was sent to number
+// Checks that user-provided code is the one that was sent to number, and if so, and if number is safe and not used before, returns credentials
 app.get("/getCredentials/:number/:code/:country", (req, res) => {
     req.setTimeout(5000); // Will timeout if no response from Twilio after 5s
 
@@ -51,7 +46,7 @@ app.get("/getCredentials/:number/:code/:country", (req, res) => {
                 .create({to: req.params.number, code: req.params.code})
                 .then(verification => {
                     if(verification.status !== "approved"){throw "There was a problem verifying the with the code provided"}
-                    getCredentials(req.params.number, req.params.country, (credentials)=>res.send(credentials))
+                    getCredentialsIfSafe(req.params.number, req.params.country, (credentials)=>res.send(credentials))
                 });
 
 })
@@ -79,12 +74,44 @@ async function credsFromNumber(phoneNumberWithPlus) {
         signature : signature
      };
 }
+
 async function signLeaf(leaf) {
     const signable = ethers.utils.arrayify(ethers.BigNumber.from(leaf));
     const wallet = new ethers.Wallet(process.env.PHONENO_ISSUER_PRIVATE_KEY);
     const signature = await wallet.signMessage(signable);
     return signature;
 }
+
+function getCredentialsIfSafe(phoneNumber, country, callback) {
+    assert(phoneNumber && country);
+    getIsSafe(phoneNumber, country, (isSafe) => {
+        if (!isSafe) throw "phone number could not be determined to belong to a unique human";
+        credsFromNumber(phoneNumber).then(creds => callback(creds));
+    });
+}
+
+function getIsSafe(phoneNumber, country, callback) {
+    assert(phoneNumber && country);
+    axios.get(`https://ipqualityscore.com/api/json/phone/${process.env.IPQUALITYSCORE_APIKEY}/${phoneNumber}?country[]=${country}`)
+    .then((response) => {
+        if(!("fraud_score" in response?.data)) {throw `Invalid response: ${JSON.stringify(response)} `}
+        console.log("number is registered: ? ", _getNumberIsRegistered(phoneNumber));
+        if(_getNumberIsRegistered(phoneNumber)) {throw "Number has been registered already!"}
+        _setNumberIsRegistered(phoneNumber);
+        callback(response.data.fraud_score <= MAX_FRAUD_SCORE)
+    })
+}
+
+
+function _setNumberIsRegistered(number, callback) {
+    connection.query(`INSERT INTO PhoneNumbers Number) VALUES ('${number}')`, (err, result) => {callback(err,result)});
+}
+
+function _getNumberIsRegistered(number, callback) {
+    connection.query(`SELECT Number FROM PhoneNumbers WHERE Number='${number}'`, (err, result) => {callback(err, result)});
+}
+
+
 
 /* - */
 app.listen(port);
