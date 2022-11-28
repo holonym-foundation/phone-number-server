1799e1c279c57ebdde4b062d783e3aa99ebe42f2
nanaknihal <nanaknihal@gmail.com>
2022-11-28 16:39:59 -0500

    fixed cors headers

--- check-number.js
+++ check-number.js
@@ -12,7 +12,7 @@ const cors = require("cors");
 const { getDateAsInt } = require("./utils.js");
 
 const app = express();
-app.use(cors({origin: ["https://holonym.id", "https://www.holonym.id","http://localhost:3000","http://localhost:3001","http://localhost:3002"]}));
+app.use(cors({origin: ["https://holonym.id", "https://www.holonym.id","https://app.holonym.id","http://localhost:3000","http://localhost:3001","http://localhost:3002"]}));
 const port = 3030;
 const MAX_FRAUD_SCORE = 75; // ipqualityscore.com defines fraud score. This constant will be used to only allow phone numbers with a <= fraud score.
 
